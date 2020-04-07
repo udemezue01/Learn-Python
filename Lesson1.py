6b0114309ee8b2787081124a95c1f10d35214f03
udemezue John <johnzeus14@gmail.com>
2020-04-07 09:10:36 +0100

    initial commit

---
+++ Lesson1.py
@@ -0,0 +1,14 @@
+'''
+
+Lesson 1
+
+
+Python crash course 
+
+Module - Print statement and variables 
+
+Reference - w3cschools.com and Sololearn App via Google play stor and Apple App Store.
+
+
+'''
+
